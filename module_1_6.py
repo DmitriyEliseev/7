8b223e81083c49e1f42124d9ffcdd4722e520271
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-07-31 12:23:38 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+# my_dict = {'Dima': 1992, 'Dany' : 2016, 'Kirill' : 2016}
+# print(my_dict)
+# print(my_dict ['Dima'])
+# my_dict['anton'] = 1994
+# my_dict.update({'max': 1996,
+#                'vova': 1993})
+# print(my_dict)
+# a = my_dict.pop('vova')
+# print(a)
+# print(my_dict)
+
+
+my_set = {1, 2, 3, 1, 2, 3, 'ok', 'pot'}
+print(my_set)
+my_set.update( {4, 5,'tok', 'cpot'})
+print(my_set)
+print(my_set.remove(1))
+print(my_set)
+
